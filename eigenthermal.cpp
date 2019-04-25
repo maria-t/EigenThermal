e9fcf6b4000e12266d59a73ae2189ba5bf392530
maria-tsiourva <tsiourva@gmail.com>
2019-04-25 12:06:32 -0700

    Create eigenthermal.cpp

---
+++ eigenthermal.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+void load_flat_images(const String &dirname, vector<Mat> &img_list, bool showImages = false)
+{
+    /* Load PGM images
+       and create a vector of flattened images 
+    */  
+    vector<String> files;
+    glob(dirname, files);
+    for (size_t i = 0; i < files.size(); ++i)
+    {
+        Mat img = imread(files[i], IMREAD_GRAYSCALE); // load the images
+        if ( img.empty() )            // invalid image, skip it.
+        {
+            cout << files[i] << " is invalid!" << endl;
+            continue;
+        }
+        if (showImages)
+        {
+            imshow("Thermal_Images", img);
+            waitKey(0);
+        }
+        img = img.reshape(0, 1); //flatten images
+        img_list.push_back(img);
+    }
+}
+
+Mat average_face(vector<Mat> &img_list, bool showAvgFace = false)
+{
+    /* Calculates the mean face */
+
+    cout << "Calculating the average route..." << endl;
+    Mat mean;
+    Mat mean_mean, stddev_mean;
+    for (int col = 0; col < img_list[0].cols; ++col)
+    {
+        int sum = 0;
+        for (int row = 0; row < img_list.size(); ++row)
+        {
+            sum += (int)img_list[row].at<uchar>(0, col);
+        }     
+        mean.push_back((double)(sum/img_list.size()));    
+    }
+
+    mean = mean.reshape(0, 512); // reshape to size of original image 48x60
+    mean.convertTo(mean, CV_8U);
+    cout<< "DONE" << endl; 
+
+    imwrite( "./Results_Thermal/AverageRoute.jpg", mean);
+    if (showAvgFace)
+        {
+            imshow("Average Route", mean);
+            waitKey(0);
+        }
+
+    return mean;
+}
+
+static Mat prepareDataForPCA(const vector<Mat> &images)
+{
+  cout << "Prepare images for PCA ..." << endl;
+  
+  Mat data(static_cast<int>(images.size()), images[0].rows * images[0].cols, CV_8U);
+  
+  // Turn an image into one row vector in the data matrix
+  for(unsigned int i = 0; i < images.size(); i++)
+  {
+    // Copy the long vector into one row of the dest
+    images[i].copyTo(data.row(i)); 
+  }
